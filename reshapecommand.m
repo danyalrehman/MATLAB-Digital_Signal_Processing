b1abbbb158105c685087a67634e54622fdb9e599
Danyal <danyal.rehman@gmail.com>
2013-06-30 17:37:32 -0400

    Reshape function

---
+++ reshapecommand.m
@@ -0,0 +1,11 @@
+% reshape command used to make a column vector into a matrix
+
+
+A = [1:12]';
+% produces a colomn vector A
+
+B = reshape(A,4,3);
+
+%produces a matrix 4x3 with values [1:4,5:8;9:12]
+
+
